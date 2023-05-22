e82dd3fd87281491956234b7fef9934f3b7316eb
Martin VANAUD <martin.vanaud@epitech.eu>
2023-05-22 17:41:25 +0200

    fix(pesto) added optionnal default parameter 'step'

--- server/pesto/pesto.py
+++ server/pesto/pesto.py
@@ -4,9 +4,13 @@ from typing import List
 from .predict import predict_from_files
 
 
-def pesto(audio_files: List[str], output_folder: str):
+def pesto(audio_files: List[str], output_folder: str, step: float = 0.10):
     model_path = os.path.join(os.getcwd(), 'models/mir-1k.pth')
-    step_size = 10.0
 
-    predictions = predict_from_files(audio_files=audio_files, model_name=model_path, output=output_folder)
+    predictions = predict_from_files(
+        audio_files=audio_files,
+        model_name=model_path,
+        output=output_folder,
+        step_size=step
+    )
     return predictions
