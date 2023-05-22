a9829dc3288f0416d6088c3995b420cc3a67f8b9
Martin VANAUD <martin.vanaud@epitech.eu>
2023-05-22 16:04:32 +0200

    fix(pesto) modified parameters and wrote path to models with os.path and cwd

--- server/pesto/pesto.py
+++ server/pesto/pesto.py
@@ -1,19 +1,12 @@
+import os
 from typing import List
 
 from .predict import predict_from_files
 
 
-# Namespace(
-#   audio_files=['../../../pesto/examples/example.wav']
-#   model_name='mir-1k'
-#   output=None
-#   export_format=['csv']
-#   reduction='alwa'
-#   step_size=10.0
-#   no_convert_to_freq=False
-#   gpu=-1
-# )
+def pesto(audio_files: List[str], output_folder: str):
+    model_path = os.path.join(os.getcwd(), 'models/mir-1k.pth')
+    step_size = 10.0
 
-def pesto(audio_files: List[str], model_path: str, output_folder: str):
     predictions = predict_from_files(audio_files=audio_files, model_name=model_path, output=output_folder)
     return predictions
