590accffcf5812c0c4ae79ccc84c600461395119
Martin VANAUD <martin.vanaud@epitech.eu>
2023-05-23 17:34:07 +0200

    fix(wrong float format) in pesto

--- server/pesto/pesto.py
+++ server/pesto/pesto.py
@@ -4,7 +4,7 @@ from typing import List
 from .predict import predict_from_files
 
 
-def pesto(audio_files: List[str], output_folder: str, step: float = 0.10):
+def pesto(audio_files: List[str], output_folder: str, step: float = 10.):
     model_path = os.path.join(os.getcwd(), 'models/mir-1k.pth')
 
     predictions = predict_from_files(
