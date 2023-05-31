75694ef0b43166debac57fd544987daa45be781a
Martin VANAUD <martin.vanaud@epitech.eu>
2023-05-31 18:03:54 +0200

    feat(FrequenceRoll component using svg) with a working time progressbar, useMemo, Tone.Transport.Schedule and much more

--- client/src/FrequenceRoll/FrequenceRoll.tsx
+++ client/src/FrequenceRoll/FrequenceRoll.tsx
@@ -1,136 +1,42 @@
 import {
-  Dispatch,
-  FC,
-  SetStateAction,
-  useEffect,
+  FC, RefObject,
+  useCallback,
+  useEffect, useMemo,
   useRef,
   useState,
 } from "react";
 import { AudioDataFromServerState } from "../types/AudioDataFromServer";
 import "./FrequenceRoll.css";
-import { CircularProgress, Slider } from "@mui/material";
 import * as Tone from "tone";
-import { AudioPlayerState } from "../types/AudioPlayerState";
 import LoadingAnimation from "../UI/LoadingAnimation";
 import RoundedTextInfo from "../UI/RoundedTextInfo";
 
 interface FrequenceRollProps {
   audioData: AudioDataFromServerState;
-
-  audioPlayerState: AudioPlayerState;
-  setAudioPlayerState: Dispatch<SetStateAction<AudioPlayerState>>;
 }
 
 interface PianoRollProps {
   audioData: AudioDataFromServerState;
+
+  height: number;
+  width: number;
+
+  time: number;
 }
 
-const PianoRoll: FC<PianoRollProps> = ({ audioData }) => {
-  const confidence = 0.7;
-  const pianoColumnRollPadding = 2;
-
-  const tones = audioData.data.map((data) => data.tone);
-  const confidences = audioData.data.map((data) => data.confidence);
-
-  const confidenceValidatedTones = tones.filter(
-    (_, i) => confidences[i] > confidence
-  );
-
-  const width = tones.length;
-  const toneMax = Math.ceil(
-    Math.max(...confidenceValidatedTones) + pianoColumnRollPadding
-  );
-  const toneMin = Math.floor(
-    Math.min(...confidenceValidatedTones) - pianoColumnRollPadding
-  );
-  const nbOfTones = toneMax - toneMin;
-
-  // Calculate the dimensions based on the number of rows (W) and columns (T)
-  const numRows = nbOfTones;
-  const numCols = width + 1;
-
-  const [cells, setCells] = useState<any[]>([]);
-
-  const getToneThird = (row: number, index: number, tone: number) => {
-    if (
-      Math.floor(tone) === Math.floor(row + toneMin) &&
-      confidences[index] > confidence
-    ) {
-      return (tone - Math.floor(tone)).toPrecision(4);
-    }
-
-    return "1";
-  };
-
-  useEffect(() => {
-    const newCells = [];
-
-    const handleClick = (key: any) => {
-      console.log(key);
-    };
-
-    for (let row = numRows; row > 0; row--) {
-      const rowCells = (
-        <tr key={`row-${row + toneMin}`}>
-          <td className="darkgrey-cell">
-            <div className="cell-text">{row + toneMin}</div>
-          </td>
-          {tones.map((tone, column) => (
-            <td
-              key={`column-${column * 0.01}`}
-              className={
-                getToneThird(row, column, tone) === (0).toPrecision(4)
-                  ? "magenta-cell-first-third"
-                  : getToneThird(row, column, tone) === (1 / 3).toPrecision(4)
-                  ? "magenta-cell-second-third"
-                  : getToneThird(row, column, tone) === (2 / 3).toPrecision(4)
-                  ? "magenta-cell-third-third"
-                  : "white-cell"
-              }
-              onClick={() =>
-                handleClick(
-                  `time: ${column * 0.01} (in seconds) note: ${row + toneMin}`
-                )
-              }
-            >
-              {/* Cell content */}
-            </td>
-          ))}
-        </tr>
-      );
-
-      newCells.push(rowCells);
-    }
-
-    setCells(newCells);
-  }, [audioData, numRows, numCols]);
-
-  return (
-    <div>
-      <table>
-        <tbody>{cells}</tbody>
-      </table>
-    </div>
-  );
-};
-
-interface PianoRoll2Props {
-  audioData: AudioDataFromServerState;
-}
-
-const PianoRoll2: FC<PianoRoll2Props> = ({ audioData }) => {
+const PianoRoll: FC<PianoRollProps> = ({ audioData, height, width, time }) => {
   const pianoColumnRollPadding = 2;
   const confidence = 0.8;
-  const blockWidth = 5;
-  const blockHeight = 5;
-  const totalWidth = audioData.data.length * blockWidth;
+  const blockWidth = 1.5;
+  const blockHeight = 10;
+  const totalWidth = width;
 
   const newData = audioData.data.map((data, index) => {
     return {
-      frequency: data.frequency,
       index: index,
-      confidence: data.confidence,
+      frequency: data.frequency,
       tone: data.tone,
+      confidence: data.confidence,
     };
   });
 
@@ -145,7 +51,6 @@ const PianoRoll2: FC<PianoRoll2Props> = ({ audioData }) => {
     (_, i) => confidences[i] > confidence
   );
 
-  const width = tones.length;
   const toneMax = Math.ceil(
     Math.max(...confidenceValidatedTones) + pianoColumnRollPadding
   );
@@ -153,84 +58,122 @@ const PianoRoll2: FC<PianoRoll2Props> = ({ audioData }) => {
     Math.min(...confidenceValidatedTones) - pianoColumnRollPadding
   );
   const nbOfTones = toneMax - toneMin;
+  const totalHeight = useMemo(() => {
+    return nbOfTones * blockHeight + (blockHeight * 4);
+  }, [nbOfTones, blockHeight])
 
-  console.log(newDataFiltered);
+  const lineRef = useRef<any>();
+  const animation = useRef<any>();
+
+  const animateLine = useCallback(() => {
+    const lineElement = lineRef.current;
+
+    if (lineElement != null) {
+      lineElement.setAttribute('x1', (time * blockWidth).toString());
+      lineElement.setAttribute('x2', (time * blockWidth).toString());
+      animation.current = requestAnimationFrame(animateLine);
+    }
+  }, [time]);
+
+  useEffect(() => {
+    if (Tone.Transport.state === "started") {
+      animation.current = requestAnimationFrame(animateLine);
+    } else {
+      cancelAnimationFrame(animation.current);
+    }
+  }, [animateLine, time]);
 
   return (
-    <svg width={totalWidth} height="100%">
-      {newDataFiltered.map((data) => (
+    <svg width={totalWidth} height={height * 0.9} >
+    {newDataFiltered.map((data) => {
+      const invertedValue = 128 - Math.floor(data.tone);
+      const rectWidth = blockWidth;
+      const rectHeight = blockHeight;
+      const rectX = (data.index * blockWidth);
+      const rectY = (invertedValue - toneMin) * blockHeight + (blockHeight / 2) - (rectHeight / 2) + ((height * 0.9) / 2);
+
+      return (
         <rect
+          className="frequence-roll-piano-note-rect"
           key={data.index}
-          x={data.index * blockWidth}
-          y={(data.tone - toneMin) * blockHeight}
-          width={10}
-          height={10}
-          fill="blue"
+          x={rectX} y={rectY}
+          width={rectWidth} height={rectHeight}
+          fill={'rgba(161,13,161,0.5)'}
+          stroke={"green"} strokeWidth={1}
         />
-      ))}
-    </svg>
+      );
+    })}
+
+      <line
+          ref={lineRef}
+          x1={time * blockWidth} x2={time * blockWidth}
+          y1={0} y2="100%"
+          stroke="red" strokeWidth={2}
+      />
+
+      <line x1={0} y1="50%" x2={totalWidth} y2="50%" stroke="red" strokeWidth={0.5}/>
+
+      {Array((width / blockWidth)).fill(null).map((_, index) => {
+        return <line
+            key={index}
+            x1={index * blockWidth * 100} x2={index * blockWidth * 100}
+            y1={"95%"} y2="100%"
+            fill="white"
+            stroke={"white"} strokeWidth={2}
+        />
+      })}
+      {Array((width / blockWidth)).fill(null).map((_, index) => {
+        return <line
+            key={index}
+            x1={index * blockWidth * 10} x2={index * blockWidth * 10}
+            y1={"98%"} y2="100%"
+            fill="white"
+            stroke={"white"} strokeWidth={1}
+        />
+      })}
+  </svg>
   );
 };
 
 const FrequenceRoll: FC<FrequenceRollProps> = ({
   audioData,
-  audioPlayerState,
-  setAudioPlayerState,
 }) => {
   const [time, setTime] = useState<number>(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(Tone.Transport.seconds);
-
-      // console.log(Tone.Transport.seconds / 100, audioData.data.length)
-      //
-      // if (Tone.Transport.seconds / 95 > audioData.data.length && Tone.Transport.state === "started") {
-      //   Tone.Transport.stop();
-      //   setAudioPlayerState((state) => {
-      //     return { ...state, isPlaying: false, currentTime: 0 };
-      //   });
-      // }
-
-      // if ((Tone.Transport.seconds % (audioData.data.length / 100)) <= 0.1) {
-      //   console.log("reset time bar");
-      //   setTime(0);
-      // } else {
-      //   setTime((Tone.Transport.seconds % (audioData.data.length / 100)));
-      // }
-    }, 100);
-    return () => clearInterval(interval);
+    Tone.Transport.scheduleRepeat(() => {
+      setTime(Tone.Transport.seconds * 95);
+    }, 0.01)
   }, []);
 
+  const frequenceRollBorderMainContainer = useRef<HTMLDivElement>(null);
+  const [containerHeight, setContainerHeight] = useState<number>(0);
+  const [containerWidth, setContainerWidth] = useState<number>(0);
+
+  useEffect(() => {
+    if (frequenceRollBorderMainContainer.current != null && audioData.isSuccessFetching) {
+      const divHeight = frequenceRollBorderMainContainer.current.offsetHeight;
+      const divWidth = frequenceRollBorderMainContainer.current.offsetWidth;
+      console.log("The height of the frequenceRollBorderMainContainer div in pixels is: " + divHeight + "px by " + divWidth + "px");
+      setContainerHeight(divHeight);
+      setContainerWidth(divWidth)
+    } else {
+      console.log("it's null");
+    }
+  }, [
+      frequenceRollBorderMainContainer.current?.offsetHeight,
+      frequenceRollBorderMainContainer.current?.offsetWidth,
+      audioData.isSuccessFetching
+  ]);
+
   return (
-    <div className="frequence-roll-border-container">
+    <div ref={frequenceRollBorderMainContainer} className="frequence-roll-border-container">
       <div className="frequence-roll-main-container">
         {audioData.isLoadingFetching ? (
           <LoadingAnimation />
         ) : audioData.data.length !== 0 ? (
           <div className="frequence-roll-piano-notes-container">
-            <PianoRoll2 audioData={audioData} />
-            {/* <Slider
-              value={time}
-              min={0}
-              max={audioData.data.length / 100}
-              sx={{
-                height: "100%",
-                padding: 0,
-                "& .MuiSlider-rail": {
-                  opacity: 0,
-                },
-                "& .MuiSlider-track": {
-                  display: "none",
-                },
-                "& .MuiSlider-thumb": {
-                  height: "100%",
-                  width: 3,
-                  borderRadius: 0,
-                  backgroundColor: "red",
-                },
-              }}
-            /> */}
+              <PianoRoll audioData={audioData} height={containerHeight} width={containerWidth} time={time} />
           </div>
         ) : (
           <RoundedTextInfo text="Waiting for a file ..." />
