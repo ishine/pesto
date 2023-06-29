f795ed7e2fa23b7a97fdaab3c57c564447e158f4
Martin VANAUD <martin.vanaud@epitech.eu>
2023-06-29 11:43:39 +0200

    feat(client app footer) added a react footer in main container App

---
+++ client/src/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-main-container">
+      <div>Designed by Martin Vanaud, Victor Palle & Sébastien Marino</div>
+    </div>
+  );
+};
+
+export default Footer;
