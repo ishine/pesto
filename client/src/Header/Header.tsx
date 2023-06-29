63dc8c7716225fbc8a34eac66d429a61823d81b4
Martin VANAUD <martin.vanaud@epitech.eu>
2023-06-29 11:44:03 +0200

    feat(client app Header) updated header component with latest data and project name

--- client/src/Header/Header.tsx
+++ client/src/Header/Header.tsx
@@ -1,8 +1,18 @@
 import { FC } from 'react'
 import "./Header.css"
+import SimpleSVGComponent from "../UI/SimpleSVGComponent";
+import PestoIcon from "../assets/play-icon.svg";
 
 const Header: FC= () => {
-  return <div className='header-main-container'>PESTO</div>
+  return (
+    <div className="header-main-container">
+      <div className="header-sony-container"></div>
+      <div className="header-pesto-container">
+        <div className="header-logo-background" />
+        PESTO
+      </div>
+    </div>
+  )
 }
 
 export default Header
