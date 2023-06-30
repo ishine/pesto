372cb2d09c6d08b9c232c027413ffc1a65e822ae
Martin VANAUD <martin.vanaud@epitech.eu>
2023-06-30 17:32:13 +0200

    feat(Footer component) added mention to Alain's work

--- client/src/Footer/Footer.tsx
+++ client/src/Footer/Footer.tsx
@@ -3,6 +3,20 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer-main-container">
+      <div>
+        {/*<a*/}
+        {/*  href="https://sites.google.com/view/samplematch?pli=1"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*  style={{*/}
+        {/*    fontSize: '12px',*/}
+        {/*    fontWeight: 200,*/}
+        {/*    color: '#FFFFFF'*/}
+        {/*  }}>*/}
+          PESTO Paper
+        {/*</a>*/}
+        &nbsp;by Alain Riou
+      </div>
       <div>Designed by Martin Vanaud, Victor Palle & Sébastien Marino</div>
     </div>
   );
